ed001359b747268b033c10d40d32964fdd1c1282
Tunisia Artope <artope@yahoo.com>
2022-02-23 12:48:08 -0500

    commit tree 002e6f9d722b63c7f8b20d1349be40ae2ea77fb9

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -3,12 +3,24 @@ import ProjectItem from "./ProjectItem";
 
 function ProjectList({ projects }) {
   console.log(projects);
+
+  const projectListItems = projects.map((project) => {
+    return <ProjectItem 
+              key={project.id}
+              name={project.name}
+              about={project.about}
+              technologies={project.technologies}
+            /> 
+  })
+
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list">{/* render ProjectItem components here */}{projectListItems}</div>
     </div>
   );
 }
 
 export default ProjectList;
+
+// within function, above return for div, create variableName and assign to map(); return ProjectItem component and add props{project.key pairs}; put {variableName} down in return div afterEach.
