778b6e28cd60420e72d590460733758d38627fe2
Tunisia Artope <artope@yahoo.com>
2022-02-23 12:48:29 -0500

    Completed assignment

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,20 @@ import React from "react";
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  // const linksElements = links.map((link) => {
+  //   return <a key={link} href={link}>{link}</a>
+  // })
+
+  return (
+        <nav>{/* display an <a> tag for each link here */}
+          {/* {linksElements} */}
+          <a href="#home">home</a>
+          <a href="#about">about</a>
+          <a href="#projects">projects</a>
+        </nav>
+  );
 }
 
 export default NavBar;
+
+// because each link is already within the function, we can hard code a tags and add link name to href.
